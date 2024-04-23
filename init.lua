751a165f3775db578695325bb665c6f715abd0e0
syevers <{ID}+{username}@users.noreply.github.com>
2024-04-22 20:58:03 -0700

    switched to lazy, save windows keybinds before merging

--- init.lua
+++ init.lua
@@ -1,1 +1,30 @@
-require('ghost')
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
+augroup END
+]]
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("core.remap")
+require("core.set")
+require("core.plugins")
+require("core.plugin_config")
